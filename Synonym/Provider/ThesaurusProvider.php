bd4414b107a9107be9f44598dab11f4a045b26bd
auooru <auooru@outlook.com>
2021-12-16 18:08:44 +0800

    update: 同义词查询引擎 [2]

---
+++ Synonym/Provider/ThesaurusProvider.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\Service\TextWord\Synonym\Provider;
+
+use app\Service\TextWord\Synonym\WordText;
+use GuzzleHttp\Promise\PromiseInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamInterface;
+use Symfony\Component\DomCrawler\Crawler;
+use function trim;
+use function urlencode;
+
+class ThesaurusProvider extends BaseProvider
+{
+
+    public static function getType(): string
+    {
+        return 'thesaurus';
+    }
+
+    protected function defaultHeaders(): array
+    {
+        return [
+            'Referer' => 'https://www.thesaurus.com/',
+        ];
+    }
+
+    protected function asyncQuery(string $word): PromiseInterface
+    {
+        // 支持原生缓存
+        $queryStr = urlencode($word);
+        return $this->client->getAsync(
+            "https://www.thesaurus.com/browse/{$queryStr}",
+        );
+    }
+
+    protected function analyze(StreamInterface $body): array
+    {
+        $firstClass = null;
+        $changeCount = 0;
+        $crawler = new Crawler($body->getContents());
+        return $crawler
+            ->filter('section.MainContentContainer > section > div > div > ul > li > a')
+            ->each(static function (Crawler $node) use (&$firstClass, &$changeCount) {
+                $class = $node->attr('class');
+                if ($firstClass === null) {
+                    $firstClass = $class;
+                } elseif ($class !== $firstClass) {
+                    $changeCount++;
+                }
+                return new WordText($node->text(), $changeCount === 0);
+            });
+    }
+
+    public function query(string $word): array
+    {
+        /** @var ResponseInterface $response */
+        $response = $this->asyncQuery($word)->wait();
+
+        $body = $response->getBody();
+
+        return $this->analyze($body);
+    }
+}
