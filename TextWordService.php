01b46610adac7dc852f2019f0b67cb8e881c9eb7
auooru <auooru@outlook.com>
2021-12-17 23:35:12 +0800

    update: 添加文本处理服务 [1]

---
+++ TextWordService.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace app\Service\TextWord;
+
+use app\Enum\WordFilterEnum;
+use app\Model\AmazonWordDictModel;
+use function count;
+use function explode;
+use function htmlspecialchars_decode;
+use function preg_match_all;
+use function preg_replace;
+use function str_replace;
+use function strip_tags;
+use function strlen;
+use function substr;
+use function trim;
+use function Zxin\Str\str_fullwidth_to_ascii;
+
+class TextWordService
+{
+    public const TYPE_WORD = 'w';
+    public const TYPE_SYMBOL = 'o';
+    public const TYPE_LF = 'lf';
+
+    public function clean(string $text): string
+    {
+        $output = htmlspecialchars_decode($text);
+        $output = strip_tags($output);
+        $output = str_replace(['&nbsp;'], [''], $output);
+        /**$output = preg_replace('#<br\s?/?>#', "\n", $output);**/
+        $output = preg_replace("/\n{2,}/u", "\n\n", $output);
+        return str_fullwidth_to_ascii($output);
+    }
+
+    public function slice(string $text): ?\Generator
+    {
+        $count = preg_match_all("/[a-z]+/i", $text, $matches, PREG_OFFSET_CAPTURE);
+
+        if (empty($count)) {
+            return null;
+        }
+
+        $lastPoint = 0;
+        foreach ($matches[0] as $match) {
+            [$word, $point] = $match;
+            $len = strlen($word);
+            if ($point > $lastPoint) {
+                $word2 = substr($text, $lastPoint, $point - $lastPoint);
+                $word2 = trim($word2, " \t\r\0\x0B");
+                if (!empty($word2)) {
+                    $t = explode("\n", $word2);
+                    if (count($t) > 1 && $t[0] === '') {
+                        // 移除切割产生的多余换行
+                        unset($t[0]);
+                    }
+                    foreach ($t as $value) {
+                        if ('' === $value) {
+                            yield [
+                                'type' => self::TYPE_LF,
+                                'stat' => null,
+                                'text' => "\n",
+                            ];
+                        } else {
+                            yield [
+                                'type' => self::TYPE_SYMBOL,
+                                'stat' => null,
+                                'text' => $value,
+                            ];
+                        }
+                    }
+                }
+            }
+            $lastPoint = $point + $len;
+            yield [
+                'type' => self::TYPE_WORD,
+                'stat' => null,
+                'text' => $word,
+            ];
+        }
+    }
+
+    public function filter(iterable $items): \Generator
+    {
+        foreach ($items as $item) {
+            ['type' => $type, 'stat' => $stat, 'text' => $text] = $item;
+
+            if (self::TYPE_WORD !== $type) {
+                yield $item;
+                continue;
+            }
+
+            $word = AmazonWordDictModel::findWord($text);
+
+            if (empty($word)) {
+                yield $item;
+                continue;
+            }
+
+            if ($word->isBad()) {
+                $item['stat'] = WordFilterEnum::_BAD;
+            } elseif ($word->isWarn()) {
+                $item['stat'] = WordFilterEnum::_WARN;
+            }
+
+            yield $item;
+        }
+    }
+
+    public function synonym(string $word): array
+    {
+        return SynonymService::instance()->queryAllWithAggregation($word);
+    }
+}
