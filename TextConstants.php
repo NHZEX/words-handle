b18560a158d81d8f7fbb36a66b1c8ebb81a1c5a3
auooru <auooru@outlook.com>
2021-12-22 23:34:21 +0800

    update: [文字处理] 解决`'`多余空格

--- TextConstants.php
+++ TextConstants.php
@@ -10,12 +10,12 @@ class TextConstants
     public const TYPE_LF     = 'lf';
 
     // 连接符，与字符结合没有空格
-    public const SYMBOL_LINK       = ['/', '′', '-', '–', '—', '=', '*', '≈', '°'];
+    public const SYMBOL_LINK       = ['/', '′', '-', '–', '—', '=', '*', '≈', '°', '\''];
     // 各种括号
     public const SYMBOL_BRACKETS_A = ['(', '[', '{'];
     public const SYMBOL_BRACKETS_B = [')', ']', '}'];
     public const SYMBOL_QUOTE            = '"';
-    public const SYMBOL_SINGLE_QUOTATION = '\'';
+    public const SYMBOL_SINGLE_QUOTATION = '\''; // todo 存在冲突
     // 分割符，与字符结合有空格
     public const SYMBOL_CUT        = [',', '.', '?', '!', ';'];
     // 分割符，切断文本分析
