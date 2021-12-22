8d517543be4236757e75e3cf8bba165e6d0e190a
auooru <auooru@outlook.com>
2021-12-23 00:30:38 +0800

    update: [文字处理] 更新符号

--- TextConstants.php
+++ TextConstants.php
@@ -10,7 +10,7 @@ class TextConstants
     public const TYPE_LF     = 'lf';
 
     // 连接符，与字符结合没有空格
-    public const SYMBOL_LINK       = ['/', '′', '-', '–', '—', '=', '*', '≈', '°', '\''];
+    public const SYMBOL_LINK       = ['/', '′', '+', '-', '–', '—', '=', '*', '≈', '°', '\''];
     // 各种括号
     public const SYMBOL_BRACKETS_A = ['(', '[', '{'];
     public const SYMBOL_BRACKETS_B = [')', ']', '}'];
