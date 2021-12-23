354edc52db4bcf6b355f4d879ae6ae786901dc50
auooru <auooru@outlook.com>
2021-12-23 17:56:49 +0800

    update: [文字处理] 支持匹配失败情况下回退

--- DictFilter.php
+++ DictFilter.php
@@ -6,6 +6,7 @@ use app\Enum\WordFilterEnum;
 use app\Model\AmazonWordDictModel;
 use app\Service\TextWord\Synonym\WordText;
 use function array_map;
+use function array_pop;
 use function array_shift;
 use function count;
 use function implode;
@@ -26,6 +27,7 @@ class DictFilter implements \IteratorAggregate
     public function getIterator(): \Traversable
     {
         $bufferWords = [];
+        $tmpWord = null;
         foreach ($this->words as $item) {
             ['type' => $type, 'text' => $text] = $item;
 
@@ -43,21 +45,29 @@ class DictFilter implements \IteratorAggregate
             }
 
             $bufferWords[] = $item;
+
+            QUERY_WORD:
             $bufferStr     = implode(' ', array_map(fn($v) => $v['text'], $bufferWords));
 
             $queryText = AmazonWordDictModel::buildQueryString($bufferStr);
             $words     = AmazonWordDictModel::findPhraseRaw($queryText, 2);
+            $matchCount = $words->count();
             if ($words->isEmpty()) {
                 // 无有效匹配
-                yield array_shift($bufferWords);
-                continue;
-            } elseif ($words->count() > 1) {
-                // 存在多个匹配 可能可以优化
-                continue;
-            } elseif ($queryText !== $words[0]['query']) {
-                // 等于1且字符串非全等
+                if (count($bufferWords) > 1) {
+                    $tmpWord  = array_pop($bufferWords);
+                    goto QUERY_WORD;
+                } else {
+                    yield array_shift($bufferWords);
+                }
+            } elseif (1 === $matchCount && $queryText !== $words[0]['query']) {
+                // 只有一个且字符串非全等
                 continue;
             } else {
+                if ($matchCount > 1 && null === $tmpWord) {
+                    // 存在多个匹配 可能可以优化
+                    continue;
+                }
                 // 等于1且字符串全等
                 $model       = $words[0];
                 if (count($bufferWords) > 1 && $bufferWords[0]['type'] !== TextConstants::TYPE_WORD) {
@@ -80,6 +90,10 @@ class DictFilter implements \IteratorAggregate
                     'text' => $text,
                     'stat' => $stat,
                 ];
+                if (null !== $tmpWord) {
+                    $bufferWords[] = $tmpWord;
+                    $tmpWord = null;
+                }
             }
         }
         if (!empty($bufferWords)) {
